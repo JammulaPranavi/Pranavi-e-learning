827fae287d0fedd94eee8b3c7a9c64bddde537f2
JammulaPranavi <jammulapranavi522@gmail.com>
2025-09-10 11:00:32 +0530

    Add files via upload

--- LessonService.java
+++ LessonService.java
@@ -3,5 +3,8 @@ package com.elearning.service;
 public class LessonService {
     public void startLesson(String lessonName) {
         System.out.println("Starting lesson: " + lessonName);
+        public void endLesson(String lessonName) {
+    System.out.println("Ending lesson: " + lessonName);
+}
     }
 }
